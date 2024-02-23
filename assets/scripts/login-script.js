b7d39402f444a1b02621f22638163c1f5ae45919
Rusma Wirawan <rusmawirawan30@gmail.com>
2024-02-23 14:05:10 +0700

    Update login-script.js
    menambahkan komentar JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,25 +1,32 @@
-/*
- * Catatan:
- * Gunakan fungsi goToHome() untuk menampilkan halaman home
- * Gunakan fungsi goToLogin() untuk menampilkan halaman login
- * Gunakan fungsi showPopUp() untuk menampilkan pop up error
- */
-
+// membuat variabel untuk setiap elemen view
 const loginFormElement = document.querySelector('#loginForm');
 const inputEmailElement = document.querySelector('#inputEmail');
 const inputPasswordElement = document.querySelector('#inputPassword');
 
+// membuat variabel untuk menyimpan informasi email dan password
 const expectedEmail = 'admin@dicoding.com';
 const expectedPassword = 'superpassword';
 
-loginFormElement.addEventListener('submit', function(event) {
-  event.preventDefault();
 
-  const email = inputEmailElement.value;
-  const password = inputPasswordElement.value;
-  if (email == expectedEmail && password == expectedPassword) {
-    goToHome();
-  } else {
-    showPopUp();
-  }
+// Menambahkan aksi klik pada button 
+loginFormElement.addEventListener('submit', function(event) {
+    event.preventDefault();
+    // TODO 1 : Mendapatkan input email dan password pengguna dari form.
+
+    // Mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan
+    const email = inputEmailElement.value;
+    const password = inputPasswordElement.value;
+  
+  
+    /* TODO 2 : Buat Logika perbandingan dengan kondisi:
+       Jika variabel email identik dengan expectedEmail dan password identik dengan expectedPassword, panggil fungsi goToHome().
+       Jika tidak, maka panggil fungsi showPopUp().
+     */
+
+    // Memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan
+    if (email == expectedEmail && password == expectedPassword){
+      goToHome(); //Jika sesuai maka program akan berpindah ke halaman home
+    } else{
+      showPopUp(); //Namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah
+    }
 });
